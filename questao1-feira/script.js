640ac8c6fd5a5712ad4eebbf6496e7e2da061303
LAUANDAAA <152909944+lauandaC@users.noreply.github.com>
2024-02-09 17:52:56 -0300

    Update script.js

---
+++ questao1-feira/script.js
@@ -0,0 +1,7 @@
+let carro=[]
+function adicionarDesconto(desconto){
+    carro.push({desconto});
+    console.log(´$desconto})
+    console.log {(descontos)} sera de 10%{preco.toFixed(2)};
+function adicionarDesconto(valorgasto)
+carro.push({valorGasto})sera de 5%{preco.ToFixed(2)}[""]
